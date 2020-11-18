27babd7b5174415beb59cc8bab4fc744a24c1191
senaarth <senaarth@gmail.com>
2020-11-18 10:21:45 -0300

    creating landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,9 +1,56 @@
-function Lading() {
-    return (
-        <div className="App">
-            <p>Landing</p>
-        </div>
-    );
-};
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default Lading;
+import './index.css';
+
+class Landing extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            instagram: '',
+            pix: ''
+        }
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const data = this.state;
+
+        try {
+            api.post('/create', data);
+        } catch(err) {
+            console.log(err);
+        }
+
+        this.setState({
+            name: '',
+            instagram: '',
+            pix: ''
+        });
+
+    }
+    
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        });
+    }
+    
+    render() {
+        return (
+            <div className="landing">
+                <form onSubmit={this.handleSubmit}>
+                    <input name="name" type="text" placeholder="Nome" value={this.state.name} onChange={this.handleInputChange}/>
+                    <input name="instagram" type="text" placeholder="Instagram" value={this.state.instagram} onChange={this.handleInputChange}/>
+                    <input name="pix" type="text" placeholder="PIX" value={this.state.pix} onChange={this.handleInputChange}/>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Landing;
