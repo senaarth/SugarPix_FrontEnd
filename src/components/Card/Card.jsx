5db2cbcd3ea2e89ee559366eb6772d3c4c38413f
Lucas <lafdesouza2002@outlook.com>
2021-01-17 01:13:04 -0300

    Terminando paginaçao

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 function Card({ key, card }) {
   const instaURL = 'https://www.instagram.com/' + card.instagram;
   const imgID = 'img' + card._id;
   const bioID = 'bio' + card._id;
+
+  // const [profilePic, setProfilePic] = useState('');
+  // async function picUrl() {
+  //   try {
+  //     const res = await fetch(instaURL + '/?__a=1');
+  //     const json = await res.json();
+  //     console.log('json', json)
+  //   } catch (err) {
+  //     console.error('err', err);
+  //   }
+  // }
+  // picUrl();
 
   function showBio() {
     const bio = document.getElementById(bioID);
@@ -22,7 +34,7 @@ function Card({ key, card }) {
   return (
     <div className='cardComponent'>
       <img
-        src='https://avatars0.githubusercontent.com/u/64823667?s=88&u=b28f3ba7506289fc5d72103289d40a40be212a11&v=4'
+        src={'https://scontent-gig2-1.cdninstagram.com/v/t51.2885-15/e35/106368633_154548092816106_2260210631900495517_n.jpg?_nc_ht=scontent-gig2-1.cdninstagram.com&_nc_cat=106&_nc_ohc=2wq7Ol40HDUAX_UPMZQ&tp=1&oh=ae9a357eee7cba9c50dc60fb40717922&oe=602ED120'}
         alt=''
         id={imgID}
       />
